8e4162fdceaa896f239047225d4b1497f51e12da
Parimal Yeole <parimal.yeole@moduscreate.com>
2022-05-23 09:23:14 +0530

    Small changes in server

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,12 @@
-import 'graphql-import-node';
-import fastify from "fastify";
-import { getGraphQLParameters, processRequest, Request, renderGraphiQL, shouldRenderGraphiQL, sendResult } from "graphql-helix";
-import { schema } from "./schema";
-import { contextFactory } from "./context";
+import { server } from "./server";
 
-
-// async function main() {
-//   const myQuery = parse(`query { info }`);
-
-//   const result = await execute({
-//     schema,
-//     document: myQuery,
-//   });
-
-//   console.log(result);
-// }
+const port = process.env.PORT || 3000;
 
 async function main() {
-  const server = fastify();
-
-  server.route({
-    method: ["POST", "GET"],
-    url: "/graphql",
-    handler: async (req, reply) => {
-      const request: Request = {
-        headers: req.headers,
-        method: req.method,
-        query: req.query,
-        body: req.body,
-      };
-
-      if (shouldRenderGraphiQL(request)) {
-        reply.header("Content-Type", "text/html");
-        reply.send(
-          renderGraphiQL({
-            endpoint: "/graphql",
-          })
-        );
-
-        return;
-      }
-
-      const { operationName, query, variables } = getGraphQLParameters(request);
-
-      const result = await processRequest({
-        request,
-        schema,
-        operationName,
-        contextFactory: () => contextFactory(req),
-        query,
-        variables,
-      });
-
-      sendResult(result, reply.raw);
-    }
+  server.listen(port, () => {
+    console.log(`GraphQL server is running on port ${port}.`);
   });
-
-  server.listen(3000, "0.0.0.0", () => {
-    console.log(`GraphQL API is running on http://localhost:3000/graphql`);
-  });
+  
 }
 
 main();
